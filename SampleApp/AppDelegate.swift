efc4d7c81e94ca215971d9e31932ae003f90c4a2
Nguyễn Việt Hà <hanvgc00472@gmail.com>
2020-04-22 10:53:50 +0700

    abc

--- SampleApp/AppDelegate.swift
+++ SampleApp/AppDelegate.swift
@@ -13,17 +13,35 @@ import AppCenterCrashes
 import AppCenterDistribute
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MSDistributeDelegate {
 
-
+    let window: UIWindow? = nil
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        MSDistribute.setDelegate(self);
+        
         // Override point for customization after application launch.
         MSDistribute.checkForUpdate()
         MSAppCenter.start("5bf8ee42-bb8e-4bd1-9746-b1cdc68cf01a", withServices:[
           MSAnalytics.self,
           MSCrashes.self
         ])
+        
+        let alertController = UIAlertController(title: "Update available.",
+                                              message: "Do you want to update?",
+                                       preferredStyle:.alert)
+
+        alertController.addAction(UIAlertAction(title: "Update", style: .cancel) {_ in
+          MSDistribute.notify(.update)
+        })
+
+        alertController.addAction(UIAlertAction(title: "Postpone", style: .default) {_ in
+          MSDistribute.notify(.postpone)
+        })
+
+        // Show the alert controller.
+        self.window?.rootViewController?.present(alertController, animated: true)
+        
         return true
     }
 
